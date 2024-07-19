6de454bcc55137569c0a9dcb95cf782cae75fb87
Cristiantovar8 <cristian.tovar1094@gmail.com>
2024-07-19 18:23:06 -0500

    Se agrega conexion a MongoDB

---
+++ src/config/mongo.ts
@@ -0,0 +1,13 @@
+import "dotenv/config"
+import { connect } from "mongoose"
+
+async function dbConnect(): Promise<void> {
+
+    //const DB_URI = <string>process.env.DB_URI
+    const DB_URI = "mongodb+srv://cristovaring8:vCIp00hdxRBP8p26@cluster0.vfrdoz5.mongodb.net/GestionTareas?retryWrites=true&w=majority&appName=Cluster0"
+    console.log(DB_URI)
+    await connect(DB_URI)
+    
+}
+
+export default dbConnect
